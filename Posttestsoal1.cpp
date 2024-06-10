f2a89e98ec08ddfe526af43428b0bbc6c48d4989
AsterionIf_nit126 <162097297+RifkaASwasthi@users.noreply.github.com>
2024-06-10 14:16:40 +0700

    Add files via upload

---
+++ Posttestsoal1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int angka;
+    angka = (1,2,3,5,5);
+    cout << "masukkan angka:" << endl;
+    cin >> angka >> endl;
+    cout << "angka berada di index:" << endl;
+    cin.get();
+    return 0;
+}
---
+++ Posttestsoal2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int faktorial;
+    int = (n!)
+    cout << "Masukkan bilangan:" << endl;
+    cin >> faktorial >> endl;
+    return 0;
+}
